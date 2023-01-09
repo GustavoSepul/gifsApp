2c0e2e27d58cb2b47c4f6b1df16ad071f963e967
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2023-01-09 16:43:51 -0300

    tipado a peticion http
    Colocando un tipado a las peticiones http mediante la creacion de una interface creada en quicktype

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,7 @@ export class GifsService {
   private apiKey: string = 'ea9NccBEvVGSgOjt33xPd9UNtDwWoqj0';
   private _historial: string[] = [];
 
-  //TODO: cambiar any por su tipo
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return this._historial;
@@ -29,8 +29,8 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=ea9NccBEvVGSgOjt33xPd9UNtDwWoqj0&q=${ query }&limit=10`)
-    .subscribe( (resp: any) =>{
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=ea9NccBEvVGSgOjt33xPd9UNtDwWoqj0&q=${ query }&limit=10`)
+    .subscribe( (resp) =>{
         console.log(resp.data);
         this.resultados = resp.data;
     });
